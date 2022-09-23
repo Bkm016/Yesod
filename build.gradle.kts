2afffa1307460effec7a1dd653cc820dbd4db08e
坏黑 <b@skymc.me>
2022-09-23 16:12:37 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     `java-library`
     `maven-publish`
-    id("io.izzel.taboolib") version "1.34"
-    id("org.jetbrains.kotlin.jvm") version "1.5.10"
+    id("io.izzel.taboolib") version "1.42"
+    id("org.jetbrains.kotlin.jvm") version "1.5.31"
 }
 
 taboolib {
